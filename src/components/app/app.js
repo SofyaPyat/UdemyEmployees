0bf869a98d456e1f3a8130b6e50a15feff13b840
Sofya <SofjaEsenina@yandex.ru>
2023-07-29 21:40:47 +0300

    +addform

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,8 @@ import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
+            <EmployeesList/>
+            <EmployeesAddForm/>
         </div>
     );
 }
