fc9ac090d65d2caff263bbab599145849e8f34a1
Sofya <SofjaEsenina@yandex.ru>
2023-07-29 19:41:13 +0300

    app + app-info

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников компании N</h1>
+            <h2>Общее число сотрудников: {2}</h2>
+            <h2>Премию получат: {0}</h2>
+        </div>
+    );
+}
+
+export default AppInfo;
