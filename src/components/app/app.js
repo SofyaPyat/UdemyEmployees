194534d510ebf1398177bdc6f294fdd205c077f3
Sofya <SofjaEsenina@yandex.ru>
2023-08-06 01:17:46 +0300

    change state from another component add+del

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,32 +1,62 @@
-import './app.css';
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+import './app.css';
 
-function App() {
+class App extends Component {
     
-    const data = [
-        {name: "Артём П.", salary: 800, increase: false, id: 1},
-        {name: "Даша Т.", salary: 2000, increase: true,  id: 2},
-        {name: "Маша Б.", salary: 5000, increase: false, id: 3}
-    ];
-    
-    return (
-        <div className="app">
-            <AppInfo/>
+    constructor (props) {
+        super(props);
+        this.state = {
+            data: [{name: "Артём П.", salary: 800, increase: false, id: 1},
+                {name: "Даша Т.", salary: 2000, increase: true,  id: 2},
+                {name: "Маша Б.", salary: 5000, increase: false, id: 3}],
+            maxId: 4
+        }
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(elem => elem.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        this.setState (({data}) => {
+            const newData = [...data];
+            newData.push({name: name, salary:salary, increase: false, id: this.state.maxId});
+            console.log(newData);
+            return {
+                data: newData,
+                maxId: this.state.maxId + 1
+             }
+        })
+    }
+    render () {
+        
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                
+                <EmployeesList data = {this.state.data}
+                               onDelete={this.deleteItem}
+                />
+                <EmployeesAddForm onAdd={this.addItem}/>  
             </div>
-            
-            <EmployeesList data = {data}/>
-            <EmployeesAddForm/>  
-        </div>
-    );
+        );
+    }
 }
 
 
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,16 +1,17 @@
 import './employees-list.css';
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
 
-    const elements = data.map((item, i) => {
+    const elements = data.map((item) => {
         const {id, ...itemProps} = item
         return (
-            <EmployeesListItem key = {i} {...itemProps} />
+            <EmployeesListItem key = {id} 
+                                     {...itemProps}
+                                     onDelete={() => onDelete(id)} />
         );
     })
 
-    console.log(elements);
 
     return (
         <ul className="app-list list-group">
